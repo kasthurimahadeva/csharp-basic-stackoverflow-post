3c25511128a5b420d31fba67f74d3c54fb5290cb
Kasthurimahadeva <kasthuri.14@cse.mrt.ac.lk>
2020-05-20 22:59:58 +0530

    basic stack-over-flow post

---
+++ Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StackOverFlowPost
+{
+    public class Post
+    {
+        public string Title { get; }
+        private string _description;
+        private DateTime _createdTime;
+        public int CurrentVote { get; private set; }
+
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            _description = description;
+            _createdTime = DateTime.Now;
+            CurrentVote = 0;
+        }
+        public void UpVote()
+        {
+            CurrentVote += 1;
+            Console.WriteLine("Successfully up-voted");
+        }
+
+        public void DownVote()
+        {
+            CurrentVote -= 1;
+            Console.WriteLine("Successfully down-voted");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StackOverFlowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Welcome to Panda-Karady StackOverFlow ***");
+            Console.WriteLine("Enter your post title");
+            var title = Console.ReadLine();
+            Console.WriteLine("Enter your post description");
+            var description = Console.ReadLine();
+            var post = new Post(title, description);
+            Console.WriteLine("Press 1 - up-vote, 2 - down-vote, 0 - exit");
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                var isNumeric = int.TryParse(userInput, out var value);
+                if (isNumeric)
+                {
+                    if (value == 1)
+                    {
+                        post.UpVote();
+                    } else if (value == 2)
+                    {
+                        post.DownVote();
+                    } else if (value == 0)
+                    {
+                        Console.WriteLine($"CurrentVote for your {post.Title} post is {post.CurrentVote}");
+                        Console.WriteLine("**********************");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not valid input. Try again...");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not valid input. Try again...");
+                }
+            }
+        }
+    }
+}
